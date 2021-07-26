a40a5dfdeec9467f9dcce9716a4ccf3ca9563edd
Jitendra Manish <jmanishm7@gmail.com>
2021-07-26 23:07:51 +0530

    Added Query Method logic to find by category ID

--- spring-boot-ecommerce/src/main/java/com/jmm/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/jmm/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.jmm.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jmm.ecommerce.entities.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
 }
